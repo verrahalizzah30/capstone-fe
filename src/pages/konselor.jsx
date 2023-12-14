637ee68b6d6d061f4a72370df84b9c1ad8a7f076
Verra Halizzah <verrahlzh@upi.edu>
2023-12-15 01:25:18 +0700

    image konslor

--- src/pages/konselor.jsx
+++ src/pages/konselor.jsx
@@ -19,43 +19,43 @@ function Konselor() {
                 <div className='d-flex flex-column gap-4'>
                   <div className='d-flex align-items-center gap-3'>
                     <CardProfil 
-                      src={"src/assets/imgKonselor/Ellipse 101.svg"} 
+                      src={"public/Ellipse 101.svg"} 
                       names={"Maron Mango S.Psi"} 
                       session={"432+ Sesi"}/>
                     <CardProfil 
-                      src={"src/assets/imgKonselor/Ellipse 101 (1).svg"} 
+                      src={"public/Ellipse 101 (1).svg"} 
                       names={"Madelyn Calzoni S.Psi"} 
                       session={"234+ Sesi"}/>
                     <CardProfil 
-                      src={"src/assets/imgKonselor/Ellipse 101 (2).svg"} 
+                      src={"public/Ellipse 101 (2).svg"} 
                       names={"Jocelyn Westervelt S.Psi"} 
                       session={"221+ Sesi"}/>
                   </div>
                   <div className='d-flex align-items-center gap-3'>
                     <CardProfil 
-                      src={"src/assets/imgKonselor/Ellipse 101 (3).svg"} 
+                      src={"public/Ellipse 101 (3).svg"} 
                       names={"Mira Bergson S.Psi"} 
                       session={"209+ Sesi"}/>
                     <CardProfil 
-                      src={"src/assets/imgKonselor/Ellipse 101 (4).svg"} 
+                      src={"public/Ellipse 101 (4).svg"} 
                       names={"Livia Saris S.Psi"} 
                       session={"178+ Sesi"}/>
                     <CardProfil 
-                      src={"src/assets/imgKonselor/Ellipse 101 (5).svg"} 
+                      src={"public/Ellipse 101 (5).svg"} 
                       names={"Carla Vaccaro S.Psi"} 
                       session={"166+ Sesi"}/>
                   </div>
                   <div className='d-flex align-items-center gap-3'>
                     <CardProfil 
-                      src={"src/assets/imgKonselor/Ellipse 101 (6).svg"} 
+                      src={"public/Ellipse 101 (6).svg"} 
                       names={"Jordyn Geidt S.Psi"} 
                       session={"129+ Sesi"}/>
                     <CardProfil 
-                      src={"src/assets/imgKonselor/Ellipse 101 (7).svg"} 
+                      src={"public/Ellipse 101 (7).svg"} 
                       names={"Maria Bergson S.Psi"} 
                       session={"97+ Sesi"}/>
                     <CardProfil 
-                      src={"src/assets/imgKonselor/Ellipse 101 (8).svg"} 
+                      src={"public/Ellipse 101 (8).svg"} 
                       names={"Ashlynn Culhane S.Psi"} 
                       session={"Baru!"}/>
                   </div>
